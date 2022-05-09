79d923fe0ee36a4ba909c4ba74a145001fb2cace
AshokKoda <a.koda59@gmail.com>
2022-05-09 11:25:53 +0530

    Added:UC1-Check employee attendence

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,10 @@
-console.log("Welcome to EmployeeWage Problems");
+/*
+ * UC1 Check Employee Present or Absent
+ */
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+    console.log("Employee is absent");
+} else {
+    console.log("Employee is present");
+}
