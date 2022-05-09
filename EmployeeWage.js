5f0118f63383eeb81f1f6a6c4f1dc643605ff4c4
AshokKoda <a.koda59@gmail.com>
2022-05-09 11:47:52 +0530

    Added:UC4 - Calculate wages for month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -13,6 +13,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 let empHrs = 0;
 empCheck = Math.floor(Math.random() * 10) % 3;
@@ -44,3 +45,13 @@ let empCheck1 = Math.floor(Math.random() * 10) % 3;
 let empHrs1 = getWorkingHours(empCheck1);
 let empWage1 = empHrs1 * WAGE_PER_HOUR;
 console.log(" Emp Hour:" + empHrs1 + " Emp Wage: " + empWage1);
+
+console.log("---------------------Monthly wage------------------------");
+let totalEmpHrs = 0;
+ for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+     let empCheck = Math.floor(Math.random() * 10) % 3;
+     totalEmpHrs += getWorkingHours(empCheck);
+ }
+ let empWage2 = totalEmpHrs * WAGE_PER_HOUR;
+console.log("Total Hours:" + totalEmpHrs + " Employee Wage:" + empWage2);
+
